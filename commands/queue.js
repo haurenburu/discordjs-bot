16a0528d371c528094463441e8815c58d2825d67
The HaureN <37116350+thehauren@users.noreply.github.com>
2018-10-04 14:10:35 -0300

    token fix

---
+++ commands/queue.js
@@ -0,0 +1,4 @@
+exports.run = async (bot, message, args, ops) => {
+    let fetched = ops.active.get(message.guild.id);
+    
+}
--- config/bot.js
+++ config/bot.js
@@ -1,3 +1,3 @@
 exports.prefix = '!';
-exports.token = 'NDk1MjI5OTM0Mzc4Mjg3MTA0.Do_FWg.hLbzmw711R4IL7HgnJvZxKz2np0';
-exports.owner = '160147786178887680';
+exports.token = '';
+exports.owner = '';
