69df745f74c84cd6487a441472ae0874c3c9e682
The HaureN <37116350+thehauren@users.noreply.github.com>
2018-10-05 15:16:14 -0300

    heroku commit

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.prefix = '!'; // prefix for bot
-exports.token = ''; // bot token from https://discordapp.com/developers/applications/
-exports.owner = ''; // id for the bot admin (for reload scrips)
+exports.token = 'NDk1MjI5OTM0Mzc4Mjg3MTA0.DpfaQA.RJrEYChGH3qLQIZ4dcQUwGstugk'; // bot token from https://discordapp.com/developers/applications/
+exports.owner = '160147786178887680'; // id for the bot admin (for reload scrips)
