4a28c8b958db8ec58d99066a17da5efce5a057b3
The HaureN <37116350+thehauren@users.noreply.github.com>
2018-10-07 00:31:52 -0300

    ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ exports.run = async (bot, message, args) => {
     // mbed for ping
     message.channel.send({embed: {
         color: 0x3b88c3,
-        description: `Pong! Latency is ${message.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms` // cal the ping end send as an embed
+        description: `Pong! ${Math.round(bot.ping)}ms` // cal the ping end send as an embed
     }});
 }
 exports.help = {
