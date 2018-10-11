f80585485febcae402d8a5f43d763b492c810713
The HaureN <37116350+thehauren@users.noreply.github.com>
2018-10-10 21:44:33 -0300

    meme command

---
+++ commands/meme.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+exports.run = async (bot, message, args, ops) => {
+    var meme = [];
+    let msg = '';
+    let delaymsg = await message.channel.send('Loading meme...');
+    if(args[0] === 'help'){
+        fs.readdir('./images/memes', (err, files) => {
+            if(err) console.log(err);
+            let memes = files.filter(f => f.split('.').pop() === 'png');
+            memes.forEach((f,i) => {
+                meme.push(f);
+            });
+            if(memes.length <= 0) console.log('No memes to load!');
+
+            for (let i = 0; i< meme.length; i++){
+                var filename = meme[i];
+                msg += ', '+path.parse(filename).name; // hello
+            }
+            delaymsg.delete();
+            message.channel.send({embed:{title: 'Meme list', color: 0xffffff, description: 'help' + msg}})
+        });
+        
+    }
+    delaymsg.delete();
+    await message.channel.send({files:[
+        {
+            attachment: `./images/memes/${args[0]}.png`,
+        }
+    ]})
+    
+
+}
+exports.help = {
+    name: 'meme',
+    desc: 'show a meme pic',
+    usage: '?meme tendachi'
+}
--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.prefix = '?'; // prefix for bot
-exports.token = process.env.token; //process.env.token bot token from https://discordapp.com/developers/applications/
-exports.owner = process.env.owner; //process.env.owner id for the bot admin (for reload scrips)
+exports.token = process.env.token; //bot token from https://discordapp.com/developers/applications/
+exports.owner = process.env.owner; //id for the bot admin (for reload scrips)
