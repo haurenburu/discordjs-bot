57a27dd6aa77f7f3f8375bda805504298089dc53
thehauren <antoniohaurensilva@gmail.com>
2018-12-10 11:36:58 -0300

    snowball command and some fixes

--- commands/snowball.js
+++ commands/snowball.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
     let Rrole = message.guild.roles.find(val => val.name === 'Snowball');
-    let Rmember = message.author;
-    // if(Rmember.roles.has(Rrole.id)) await Rmember.removeRole(Rrole.id);
-    
+   
     if (message.member.roles.has(Rrole.id)){
         await message.member.removeRole(Rrole.id)
+        message.reply('You will be notified now');
     }
     else { 
         await message.member.addRole(Rrole.id)
+        message.reply('You will not be notified now');
     }
 
 }
