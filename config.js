bb06ea438a2cfa2e3b7c9d3813365ccab87bd9c3
thehauren <antoniohaurensilva@gmail.com>
2018-12-10 11:33:43 -0300

    snowball command and some fixes

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.prefix = '?'; // prefix for bot
-exports.token = 'MzAzNTExNjYwODU1ODIwMjg4.DvADrA.n2Vzm59FKnq_yidm8aKBKwOfRDQ'//process.env.token; //bot token from https://discordapp.com/developers/applications/
+exports.prefix = '!'; // prefix for bot
+exports.token = process.env.token; //bot token from https://discordapp.com/developers/applications/
 exports.owner = process.env.owner; //id for the bot admin (for reload scrips)
