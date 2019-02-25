5c39d2b664c7bb39f37bb8466cf935542e6e6a51
thehauren <antoniohaurensilva@gmail.com>
2019-02-25 13:23:09 -0300

    pharmacy command]

---
+++ commands/pharmacy.js
@@ -0,0 +1,53 @@
+exports.run = async (bot, message, args, ops) => {
+    let items = {
+        shp: {
+            ID: 12422,
+            name: "Increase HP Potion (Small)",
+            ingredient1: {id: 713, name: "Empty Bottle", quant: 10},
+            ingredient2: {id: 528, name: "Monster Feed", quant: 5},
+            ingredient3: {id: 508, name: "White Herb", quant: 10},
+            ingredient4: {id: 7455, name: "Spicy Sauce", quant: 1}
+        },
+        mhp: {
+            ID: ,12423
+            name: "Increase HP Potion (Medium)",
+            ingredient1: {id: 713, name: "Empty Bottle", quant: 10},
+            ingredient2: {id: 508, name: "White Herb", quant: 10},
+            ingredient3: {id: 508, name: "Yellow Herb", quant: 10},
+            ingredient4: {id: 7455, name: "Spicy Sauce", quant: 1}
+        },
+        lhp: {
+            ID: 12424,
+            name: "	Increase HP Potion (Large)",
+            ingredient1: {id: 713, name: "Empty Bottle", quant: 10},
+            ingredient2: {id: 508, name: "White Herb", quant: 15},
+            ingredient3: {id: 522, name: "Mastela Fruit", quant: 3},
+            ingredient4: {id: 523, name: "Holy Water", quant: 1},
+            ingredient5: {id: 7455, name: "Spicy Sauce", quant: 1}
+        },
+        sp: {
+            ID: 12425,
+            name: "Increase SP Potion (Small)",
+            ingredient1: {id: 713, name: "Empty Bottle", quant: 10},
+            ingredient2: {id: 568, name: "Lemon", quant: 10},
+            ingredient3: {id: 514, name: "Grape", quant: 10},
+            ingredient4: {id: 7453, name: "Sweet Sauce", quant: 1}
+        }
+        mp: {
+            ID: 12426,
+            name: "Increase SP Potion (Medium)",
+            ingredient1: {id: 713, name: "Empty Bottle", quant: 10},
+            ingredient2: {id: 518, name: "Honey", quant: 10},
+            ingredient3: {id: 510, name: "Blue Herb", quant: 10},
+            ingredient4: {id: 7453, name: "Sweet Sauce", quant: 1}
+        }
+        lsp: {
+            ID: 12427,
+            name: "	Increase SP Potion (Large)",
+            ingredient1: {id: 713, name: "Empty Bottle", quant: 10},
+            ingredient2: {id: 526, name: "Royal Jelly", quant: 10},
+            ingredient3: {id: 510, name: "Blue Herb", quant: 15},
+            ingredient4: {id: 7453, name: "Sweet Sauce", quant: 1}
+        }
+    }
+}
